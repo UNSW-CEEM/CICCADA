fe8e871296fd6a22a297a47457ee665876691344
Hossein-Saberi <84210875+Hossein-Saberi@users.noreply.github.com>
2025-02-21 09:31:50 +1100

    Update array_job.sh

--- katana_template/array_job.sh
+++ katana_template/array_job.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=01:59:00
 #PBS -j oe
 #PBS -o ./Log/array_job.log
-#PBS -J 1-10 # this isthe range of array jobs, here you ask to run job 1 to job 1200
+#PBS -J 1-10 # this isthe range of array jobs, here you ask to run job 1 to job 10
 item=$(sed -n "${PBS_ARRAY_INDEX} p" "corresponding_data_file_list.txt") # This line and the following one are useful when you want to pass on a specific file to each array job
 read -r data_file_j <<<"${item}" #  
 module load tensorflow/2.11.0
