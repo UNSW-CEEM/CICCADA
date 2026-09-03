6e06f3c8bd66a368656af6b1559ff21bfb082b7f
ShubhankarKapoor <shubhankarkapoor94@gmail.com>
2026-09-04 03:03:46 +1000

    removed the local version of full run on SolA dataset

--- LSO_anti_islanding/conformance/solar_analytics_workflow/trino/run_evm_trino_locally_on_lso_ov_sites.py
+++ LSO_anti_islanding/conformance/solar_analytics_workflow/trino/run_evm_trino_locally_on_lso_ov_sites.py
@@ -1,4 +1,5 @@
 # script good for plotting and saving plots locally
+#
 import sys
 from datetime import datetime
 from pathlib import Path
--- LSO_anti_islanding/conformance/solar_analytics_workflow/trino/run_evm_trino_on_ec2_on_lso_ov_sites_daily.py
+++ LSO_anti_islanding/conformance/solar_analytics_workflow/trino/run_evm_trino_on_ec2_on_lso_ov_sites_daily.py
@@ -1,7 +1,12 @@
-# This script is to be run on the EC2 instance for daily LSO anti-islanding
+# This script is to be run on the EC2 instance for reporting 
+# per day per site LSO anti-islanding cnofromance
+# it is a subset of the bigger ec2 script
+# helpful to run if the cohort of assessed sites wont change but 
+# the method is slightly changed
 # conformance results for the assessed Solar Analytics site cohort.
 
-# download the table lso_anti_islanding_conformance in csv to run -- needs verification
+# think if you want this to be the script to insert into tables
+# so tables are insertee only for assessed sites or the big ec2 script
 import sys
 from datetime import datetime
 from pathlib import Path
